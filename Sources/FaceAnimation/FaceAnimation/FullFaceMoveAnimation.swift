6873e631fc12968ece4ba2d546002e1a2b6b72cd
Alex C <acoundou@gmail.com>
2023-09-11 14:23:51 -0400

    Added full face animation protocols.

---
+++ Sources/FaceAnimation/FaceAnimation/FullFaceMoveAnimation.swift
@@ -0,0 +1,45 @@
+//
+//  FullFaceMoveAnimation.swift
+//
+//
+//  Created by Alex Coundouriotis on 9/11/23.
+//
+
+import Foundation
+
+fileprivate struct Move: MoveAnimation {
+    var moveToPosition: CGPoint
+}
+
+protocol FullFaceMoveAnimation: FaceAnimation {
+    
+    var moveToPosition: CGPoint { get set }
+    
+}
+
+extension FullFaceMoveAnimation {
+    
+    var eyebrowsAnimation: FacialFeatureAnimation? {
+        Move(moveToPosition: moveToPosition)
+    }
+    
+    var eyesAnimation: FacialFeatureAnimation? {
+        Move(moveToPosition: moveToPosition)
+    }
+    
+    var noseAnimation: FacialFeatureAnimation? {
+        let newMoveToPosition = CGPoint(x: moveToPosition.x / 2, y: moveToPosition.y / 2)
+        return Move(moveToPosition: newMoveToPosition)
+    }
+    
+    var mouthAnimation: FacialFeatureAnimation? {
+        let newMoveToPosition = CGPoint(x: moveToPosition.x / 2, y: moveToPosition.y / 2)
+        return Move(moveToPosition: newMoveToPosition)
+    }
+    
+    var backgroundAnimation: FacialFeatureAnimation? {
+        let newMoveToPosition = CGPoint(x: moveToPosition.x / 4, y: moveToPosition.y / 4)
+        return Move(moveToPosition: newMoveToPosition)
+    }
+    
+}

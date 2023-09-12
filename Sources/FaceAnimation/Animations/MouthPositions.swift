61f458b9a2dd1e413fcfbfcd8db24a89a07d9f45
Alex C <acoundou@gmail.com>
2023-09-12 02:06:01 -0400

    Changed eyebrow and mouth positions

--- Sources/FaceAnimation/Animations/MouthPositions.swift
+++ Sources/FaceAnimation/Animations/MouthPositions.swift
@@ -16,7 +16,7 @@ public enum MouthPositions: LineAnimation {
     
     public func getLinePosition(width: CGFloat, height: CGFloat) -> CGPoint {
         switch(self) {
-        case .smile: return CGPoint(x: width * 19 / 50, y: height * 19 / 25)
+        case .smile: return CGPoint(x: width * 19 / 50, y: height * 56 / 75)
         case .neutral: return CGPoint(x: width * 61 / 150, y: height * 19 / 25)
         case .frown: return CGPoint(x: width * 59 / 150, y: height * 19 / 25)
         case .thinking: return CGPoint(x: width * 31 / 75, y: height * 113 / 150)
@@ -34,7 +34,7 @@ public enum MouthPositions: LineAnimation {
 
     public func getQuadCurveControlPoint(width: CGFloat, height: CGFloat) -> CGPoint {
         switch(self) {
-        case .smile: return CGPoint(x: width * 1 / 2, y: height * 58 / 75)
+        case .smile: return CGPoint(x: width * 1 / 2, y: height * 19 / 25)
         case .neutral: return CGPoint(x: width * 1 / 2, y: height * 58 / 75)
         case .frown: return CGPoint(x: width * 1 / 2, y: height * 23 / 30)
         case .thinking: return CGPoint(x: width * 17 / 30, y: height * 56 / 75)
